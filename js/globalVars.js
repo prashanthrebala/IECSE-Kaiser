96f3c6d584db6ebca81e9f9a93caba4d0d318c99
Prashanth Rebala <prashanth.rebala@gmail.com>
2017-12-31 02:03:20 +0530

    Imported Superheroes Template

---
+++ js/globalVars.js
@@ -0,0 +1,201 @@
+
+var currentQuestion = 0;
+var duration = 90;
+var numberOfQuestions = 20;
+var defaultTries = [100, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+						 7, 7, 7, 7, 7, 7, 7, 9, 9, 9];
+
+var participant = 
+
+{
+	'startTimeStamp': 0,
+	'endTimeStamp' : 0,
+	'score': 0,
+	'submissionHistory': [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+};
+
+// var ngui = require('nw.gui');
+// var nwin = ngui.Window.get();
+// var DataStore = require('nedb');
+// var path = require('path');
+// var db = new DataStore(
+// {
+// 	filename : path.join(ngui.App.dataPath, 'Kaiser.db'),
+// 	autoload : true
+// });
+
+function isSpaceChar(char){ return (char < 33 || char > 126);}
+
+function dropSpaceChars(string)
+{
+	var x = 0, y = string.length - 1;
+	while(isSpaceChar(string.charCodeAt(x))) x++;
+	while(isSpaceChar(string.charCodeAt(y))) y--;
+	return string.substr(x, y - x + 1);
+}
+
+/*
+Question Link Format:
+<div id="qn13" class="questionLink" onclick="displayQuestion(13)">
+	<div id="qn13T" class="questionNumber">
+		Question 13
+	</div>
+	<div id="qn13S" class="questionStatus">
+		7
+	</div>
+</div>
+*/
+
+function setVariables()
+{
+	var questionLinksHTML = "<hr style='width: 100%;'>";
+	for(let i=1;i<=numberOfQuestions;i++)
+	{
+		questionLinksHTML += "<div id='qn" + i + "' class='questionLink' onclick='displayQuestion("+i+")'>";
+		questionLinksHTML += "<div id='qn" + i + "T' class='questionNumber'>Question " + i + "</div>";
+		questionLinksHTML += "<div id='qn" + i + "S' class='questionStatus'>" + (questions[i]['solved'] ? '&#10003;' : questions[i]['attempts']) + "</div>";
+		questionLinksHTML += "</div>" + (i == numberOfQuestions ? "<hr style='width: 100%;'>" : "<hr>");
+	}
+	$('#sideBarID').html(questionLinksHTML);
+
+	for(let i=1;i<=numberOfQuestions;i++)
+	{
+		if(questions[i]['solved'])
+			$("#qn" + i + "S").css({'border' : '2px solid #37B76C', 'color' : '#37B76C'});
+		else if(questions[i]['attempts'] < defaultTries[i])
+			$("#qn" + i + "S").css({'border' : '2px solid #FF3F2F', 'color' : '#FF3F2F'});
+	}
+
+	$('#countDown').countdown(participant['endTimeStamp'])
+	.on('update.countdown', function(event) 
+	{
+		var format = '%H:%M:%S';
+		$(this).html(event.strftime(format));
+	})
+	.on('finish.countdown', function(event) 
+	{
+		$(this).text("00:00:00");
+			//uncomment while deploying
+			// $("#submitButton").css("pointer-events","none");
+			// alert('Your time\'s up!');
+	});
+	// nwin.show();
+	// nwin.maximize();
+
+}
+
+function displayQuestion(n)
+{
+	currentQuestion = n;
+	$('#appHeaderID').text("Question " + currentQuestion);
+	$('#questionDescriptionID').html(questions[currentQuestion]['questionStatement']);
+	$('#answerText').val('');
+}
+
+function openNav()
+{ 
+	$('#myScore').text(participant['score']);
+	$('#mySidenav').css({'width' : '23%', 'transition' : '0.5s'});
+}
+
+function closeNav()
+{ 	$('#mySidenav').css({'width' : '0', 'transition' : '0.3s'});}
+
+function submit()
+{
+	submitX(function()
+	{
+		// db.remove({}, { multi: true }, function (err, numRemoved) 
+		// {
+		// 	db.insert(
+		// 	{
+		// 		participant: participant,
+		// 		questions: questions
+		// 	}, function(err, newDocs){
+		// 		console.log(err);
+		// 		console.log(newDocs);
+		// 	});
+		// });
+	});
+}
+
+function submitX(callback) 
+{
+	var typedAnswer = $('#answerText').val();
+
+	if(currentQuestion <= 0 || questions[currentQuestion]['solved'] || questions[currentQuestion]['attempts'] <= 0)
+		return callback();
+
+	questions[currentQuestion]['attempted'] = true;
+
+	var id = "#qn"+currentQuestion+"S";
+
+	typedAnswer = dropSpaceChars(typedAnswer);
+
+	if(typedAnswer.length <= 0)
+		return callback();
+
+	if(participant['submissionHistory'][currentQuestion].indexOf(typedAnswer) >= 0)
+		if(!confirm("You have already submitted this answer for this question. Are you sure you want to submit again?"))
+			return callback();
+
+	participant['submissionHistory'][currentQuestion].push(typedAnswer);
+
+	if(Sha256.hash(typedAnswer) === questions[currentQuestion]['answer']) 
+	{
+		$('#successModal').delay(100).fadeIn();
+		$('#successModal').delay(300).fadeOut();
+		questions[currentQuestion]['attempts'] = -1;
+		$(id).css({'border' : '2px solid #37B76C', 'color' : '#37B76C'});
+		$(id).html('&#10003;');
+		questions[currentQuestion]['solved'] = true;
+		participant['score'] += questions[currentQuestion]['score'];
+		$('#sDinner2').text(participant['score']);
+		callback();
+	}
+	else if(questions[currentQuestion]['attempts'] > 0)
+	{
+		$('#wrongAnswerModal').delay(100).fadeIn();
+		$('#wrongAnswerModal').delay(300).fadeOut();
+		questions[currentQuestion]['attempts']--;
+		$(id).css({'border' : '2px solid #FF3F2F', 'color' : '#FF3F2F'});
+		$(id).text(questions[currentQuestion]['attempts']);
+		callback();
+	}
+
+}
+
+function launchApp()
+{
+	// db.find({}, function(err, docs)
+	// {
+	// 	if(docs.length == 0)
+	// 	{
+			
+			participant['startTimeStamp'] = new Date().getTime();
+			participant['endTimeStamp']   = participant['startTimeStamp'] + duration * 60000;
+	// 		db.insert(
+	// 		{
+	// 			participant: participant,
+	// 			questions: questions
+	// 		},function(err, newDocs){	setVariables();	});
+	// 	}
+	// 	else
+	// 	{
+	// 		// console.log(docs[0]);
+	// 		participant = docs[0].participant;
+	// 		questions   = docs[0].questions;
+	// 		$('#sDinner2').text(participant['score']);
+	// 		setVariables();
+	// 	}
+	// });
+}
+
+
+$(document).ready(function() 
+{
+	try{ launchApp(); } 
+	catch(err){ console.log(err); }
+});
+// document.addEventListener('contextmenu', event => event.preventDefault());
+
---
+++ js/questions.js
@@ -0,0 +1,248 @@
+
+var le = '&le;', ge = '&ge;', lt = '&#60;', gt = '&#62;';
+
+var questions = 
+
+	{
+
+		/* 1 */
+		1 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 20,
+			'answer' : ""
+
+		},
+
+		/* 2 */
+		2 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 20,
+			'answer' : ""
+
+		},
+
+		/* 3 */
+		3 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 20,
+			'answer' : ""
+
+		},
+
+		/* 4 */
+		4 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 20,
+			'answer' : ""
+
+		},
+
+		/* 5 */
+		5 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 20,
+			'answer' : ""
+
+		},
+
+		/* 6 */
+		6 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 20,
+			'answer' : ""
+
+		},
+
+		/* 7 */
+		7 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 30,
+			'answer' : ""
+
+		},
+
+		/* 8 */
+		8 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 30,
+			'answer' : ""
+
+		},
+
+		/* 9 */
+		9 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 30,
+			'answer' : ""
+
+		},
+
+		/* 10 */
+		10 : {
+
+			'questionStatement' : "",
+			'attempts' : 5,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 30,
+			'answer' : ""
+
+		},
+
+		/* 11 */
+		11 : {
+
+			'questionStatement' : "",
+			'attempts' : 7,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 50,
+			'answer' : ""
+
+		},
+
+		/* 12 */
+		12 : {
+
+			'questionStatement' : "",
+			'attempts' : 7,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 50,
+			'answer' : ""
+
+		},
+
+		/* 13 */
+		13 : {
+
+			'questionStatement' : "",
+			'attempts' : 7,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 50,
+			'answer' : ""
+
+		},
+
+		/* 14 */
+		14 : {
+
+			'questionStatement' : "",
+			'attempts' : 7,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 50,
+			'answer' : ""
+
+		},
+
+		/* 15 */
+		15 : {
+
+			'questionStatement' : "",
+			'attempts' : 7,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 50,
+			'answer' : ""
+
+		},
+
+		/* 16 */
+		16 : {
+
+			'questionStatement' : "",
+			'attempts' : 7,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 50,
+			'answer' : ""
+
+		},
+
+		/* 17 */
+		17 : {
+
+			'questionStatement' : "",
+			'attempts' : 7,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 50,
+			'answer' : ""
+
+		},
+
+		/* 18 */
+		18 : {
+
+			'questionStatement' : "",
+			'attempts' : 9,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 80,
+			'answer' : ""
+
+		},
+
+		/* 19 */
+		19 : {
+
+			'questionStatement' : "",
+			'attempts' : 9,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 80,
+			'answer' : ""
+
+		},
+
+		/* 20 */
+		20 : {
+
+			'questionStatement' : "",
+			'attempts' : 9,
+			'solved' : false,
+			'attempted' : false,
+			'score' : 80,
+			'answer' : ""
+
+		}
+
+	};
